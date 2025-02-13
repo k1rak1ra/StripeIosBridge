ab326fb4f20b43e04e97d537242cd5c02cab2ddd
Ollie Gaus <Oliver_Gaus@gap.com>
2025-02-13 00:32:46 -0500

    Initial Commit

---
+++ StripeIosBridge/StripeIosBridge.h
@@ -0,0 +1,18 @@
+//
+//  StripeIosBridge.h
+//  StripeIosBridge
+//
+//  Created by Oliver Gaus on 2025-02-13.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StripeIosBridge.
+FOUNDATION_EXPORT double StripeIosBridgeVersionNumber;
+
+//! Project version string for StripeIosBridge.
+FOUNDATION_EXPORT const unsigned char StripeIosBridgeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StripeIosBridge/PublicHeader.h>
+
+
