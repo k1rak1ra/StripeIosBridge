a315ae5c46e6481f73152ce1a40c2f7f1a2a1578
Ollie Gaus <k1ra@k1ra.net>
2025-02-15 20:26:44 -0500

    first commit

---
+++ StripeIosBridge/StripePaymentSheet.m
@@ -0,0 +1,8 @@
+//
+//  StripePaymentSheet.m
+//  StripeIosBridge
+//
+//  Created by Oliver Gaus on 2025-02-13.
+//
+
+#import <Foundation/Foundation.h>
---
+++ StripeIosBridge/StupidBridge.swift
@@ -0,0 +1,8 @@
+//
+//  StupidBridge.swift
+//  StripeIosBridge
+//
+//  Created by Oliver Gaus on 2025-02-13.
+//
+
+import Foundation
